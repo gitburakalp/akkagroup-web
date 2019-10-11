40dfadf6be974bf85dcb0aa46a99c4db42b99402
Burak Alp <burakalp17@gmail.com>
2019-10-11 15:30:02 +0300

    last changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,30 @@
-var urlPathName = window.location.pathname;
-var lang = localStorage.getItem("lang");
-var ConstructionList = [];
-var ConstructionUrl =
-  lang == null
-    ? "/contents/construction-projects-tr.json"
-    : "/contents/construction-projects-en.json";
+const Header =
+  '<nav class="header-nav"> <a href="/new" class="header-logo"> <img src="/images/svg/akka-logo.svg" alt="" title="" /> </a><div class="hamburger d-md-none"><div></div><div></div><div></div></div><ul class="header-menu"></ul> </nav>';
 
-var TravelUrl =
-  lang == null ? "/contents/travel-tr.json" : "/contents/travel-en.json";
+const Footer =
+  '<div class="footer-container"><div class="row"><div class="col-12"><h2 class="footer-title">CONTACT</h2></div></div><div class="row align-items-center text-center text-xl-left"><div class="col-12 col-xl-auto"><h4 class="footer-subtitle"> AKKA Head Office</h4><p class="footer-description"> Tunalı Hilmi Cad. 114/ 36<br /> 06700 Ankara<br /><br /> T. +90 312 467 44 91<br /> F. +90 312 467 14 15</p></div><div class="col-12 col-xl-auto"><h4 class="footer-subtitle"> AKKA Head Office</h4><p class="footer-description"> Şilli Meydanı Güneş Sok. 4 / 4<br /> Ankara<br /><br />T. +90 312 467 14 14<br /> F. +90 321 467 30 60</p></div><div class="col-12 col-xl-auto"><h4 class="footer-subtitle"> AKKA İstanbul Office</h4><p class="footer-description"> FSM caddesi Özgür Sok. Hilal Plaza No :22 <br />Kat :3 Kavacık Beykoz İstanbul<br /><br />T. +90 216 425 09 15 <br /> F. +90 216 425 09 85</p></div><div class="col-12 col-xl-auto"><h4 class="footer-subtitle"> AKKA Head Office</h4><p class="footer-description"> AKKA Hotels Antedon De Luxe<br /> Beldibi İlköğretim Okulu Yanı Beldibi 1 <br /> 07985 Kemer Antalya<br /><br />T. +90 242 824 99 99<br /> F. +90 242 824 98 91</p></div><div class="order--1 input-section ml-xl-auto col-12 col-xl-auto p-0"> <input type="text" class="form-control--transparent" placeholder="Name" /> <input type="text" class="form-control--transparent" placeholder="Surname" /> <input type="text" class="form-control--transparent" placeholder="E-Mail" /><textarea rows="3" class="form-control--transparent" placeholder="Message" ></textarea><div class="col-12 text-center"> <a href="#" class="btn btn-primary">SEND</a></div></div></div><ul class="footer-menu mt-5"></ul><div class="d-flex flex-wrap align-items-center justify-content-center akka-group-section" ><div class="col-12 text-center col-xl-auto"> <img class="img-fluid" src="/images/svg/footer-logo.svg" alt="" /></div><div class="col-12 col-xl-auto"><p class="footer-copyright"> All rights reserved. 2019 © AKKA Group</p></div></div></div>';
 
-var switchLang = "";
+const ProjectDetailsPage =
+  '<section class="pd-section"><div class="pd-section__headings"><h3 class="project-details__title" project-details-title></h3><h4 class="project-details__subtitle" project-type> Project Type</h4><ul class="breadcrumb"><li class="breadcrumb-item"> <a href="/" class="breadcrumb-link" home-title></a></li><li class="breadcrumb-item" project-type></li></ul></div><div class="row no-gutters pd-section__details justify-content-md-center" ><div class="order-2 order-xl-1 col-auto"><div class="row mx-0 mt-5 mt-lg-0 align-items-center justify-content-between" ><div class="col-auto pr-0 pd-section__logo"> <img class="img-fluid" src="/images/svg/akka-suites-logo.svg" /></div><div class="col-auto"><h2 class="project-title text-right mb-md-105" project-title ></h2></div></div><div class="row mx-0 mt-5"><div class="col-12"><p class="description text-left" project-description></p><p class="description text-left" project-description-2></p></div></div><div class="row mx-0"><div class="col-12"><ul class="fw-section-list is-shown is-black"><li class="fw-section-list__item"></li></ul></div></div></div><div class="order-1 order-xl-2 col-auto"><div class="slider-container" data-elem="swiper"><div class="slider-wrapper"><div class="slider-slide"> <img class="img-fluid" src="https://via.placeholder.com/950x400" alt="Image" /></div><div class="slider-slide"> <img class="img-fluid" src="https://via.placeholder.com/950x400" alt="Image" /></div><div class="slider-slide"> <img class="img-fluid" src="https://via.placeholder.com/950x400" alt="Image" /></div></div><div class="slider-pagination"></div></div><div class="row mx-auto"><div class="col-12 text-right"> <a class="btn" next-project><b>Next</b> Project</a></div></div></div></div> </section> <a href="" class="btn--back"></a>';
+
+$("#projectDetailsPage").each(function() {
+  $(this).append(ProjectDetailsPage);
+});
+
+let urlPathName = window.location.pathname;
+
+let lang =
+  localStorage.getItem("lang") === null ? "tr" : localStorage.getItem("lang");
+let switchLang = lang === "tr" ? "en" : "tr";
+
+let ConstructionList = [];
+
+let ConstructionUrl = `/contents/construction-projects-${lang}.json`;
+let TravelUrl = `/contents/travel-${lang}.json`;
+let siteJSON = null;
+
+$(".header").append(Header);
+$(".footer").append(Footer);
 
 var headerNameList = {
   tr: [
@@ -26,25 +41,101 @@ var headerNameList = {
   ]
 };
 
-$.each(headerNameList, function(i, e) {
-  var currentLang = lang != null ? lang.toLowerCase() : "tr";
-  if (i == currentLang) {
-    $.each(e, (i, j) => {
-      $(".header-menu").append(
-        `<li class="header-menu__item"><a href="${j.path}" class="header-menu__link">${j.name}</a></li>`
-      );
-    });
-  }
-});
-
-var TravelList = [
+let TravelList = [
   "AKKA HOTELS Antedon",
   "AKKA HOTELS Alinda",
   "AKKA HOTELS Claros",
   "AKKA HOTELS Residence"
 ];
 
+//#region Load Site Content
+
+$(function() {
+  $.ajax({
+    type: "get",
+    url: "/contents/site.json",
+    contentType: "application/json",
+    dataType: "json",
+    async: false,
+    success: function(response) {
+      $.each(response, function(jsonLang, e) {
+        var currentLang = lang != "" ? lang.toLowerCase() : "tr";
+
+        if (jsonLang == currentLang) {
+          $.each(e, function(type, elem) {
+            siteJSON = e;
+
+            switch (type) {
+              case "about":
+                $("[about-title]").html(elem.title);
+                $("[about-subtitle]").html(elem.subtitle);
+                $("[about-content]").html(elem.content);
+                break;
+              case "qualityAssurance":
+                $("[qualityAssurance-title]").html(elem.title);
+                $("[qualityAssurance-content]").html(elem.content);
+                break;
+              default:
+                $("[project-details-title]").html(siteJSON.projectTitle);
+                $("[project-subtitle]").html("Project Type");
+                $("[home-title]").html(siteJSON.homeTitle);
+
+                break;
+            }
+          });
+        }
+      });
+    },
+    failure: function(response) {
+      console.log(response);
+    }
+  });
+});
+
+//#endregion
+
+//#region Header Functions
+$.each(headerNameList, function(i, e) {
+  var currentLang = lang != "" ? lang.toLowerCase() : "tr";
+  if (i == currentLang) {
+    $.each(e, (i, j) => {
+      $(".header-menu").append(
+        `<li class="header-menu__item"><a href="${j.path}" class="header-menu__link">${j.name}</a></li>`
+      );
+
+      $(".footer-menu").append(
+        `<li class="footer-menu__item"><a href="${j.path}" class="footer-menu__link">${j.name}</a></li>`
+      );
+    });
+  }
+});
+
+$(
+  `<li class="header-menu__item"><a class="header-menu__link" lang-btn></a></li>`
+).insertAfter(".header .header-menu__item:last-child");
+
+$("[lang-btn]").each(function() {
+  switch (lang) {
+    case "tr":
+      $(this).text(switchLang);
+      break;
+    case "en":
+      $(this).text(switchLang);
+      break;
+  }
+
+  document.title = "AKKA GROUP | " + lang;
+
+  $(this).on("click", function() {
+    localStorage.setItem("lang", switchLang);
+    window.location.href = window.location.href;
+  });
+});
+//#endregion
+
 $(".main-slider").each(function() {
+  console.log(siteJSON);
+
   const mainSliderItems = [
     "Akka<br><small>Tourism</small>",
     "Akka<br><small>Construction</small>",
@@ -104,32 +195,6 @@ $(".main-slider").each(function() {
       .hasClass("is-active")
       ? $(".main-slider").removeClass("is-active")
       : "";
-  });
-});
-
-$(
-  `<li class="header-menu__item"><a class="header-menu__link" lang-btn></a></li>`
-).insertAfter(".header .header-menu__item:last-child");
-
-$("[lang-btn]").each(function() {
-  switch (lang) {
-    case "TR":
-      lang = "TR";
-      switchLang = "EN";
-      $(this).text(switchLang);
-      break;
-    case "EN":
-      lang = "EN";
-      switchLang = "TR";
-      $(this).text(switchLang);
-      break;
-  }
-
-  document.title = "AKKA GROUP | " + lang;
-
-  $(this).on("click", function() {
-    localStorage.setItem("lang", switchLang);
-    window.location.href = window.location.href;
   });
 });
 
@@ -513,6 +578,7 @@ if (lang != undefined) {
     success: function(response) {
       $.each(response, function(i, e) {
         if (e.Url == urlPathName) {
+          console.log(ConstructionUrl);
           $("[project-title]").html(e.Title);
           $("[project-description]").html(e.Description);
           $("[project-description-2]").html(e.Description2);
